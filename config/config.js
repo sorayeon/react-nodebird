0b8f4bb0d92bbfb6d5f6cce0b9dc038acc81890f
jihwa <jihwan77@gmail.com>
2021-01-17 12:57:50 +0900

    cookie sharing

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 const prod = process.env.NODE_ENV === 'production';
 module.exports = {
-  backendUrl: prod ? 'http://3.36.99.161' : 'http://localhost:3065',
+  backendUrl: prod ? 'http://api.sorayeon.shop' : 'http://localhost:3065',
 };
