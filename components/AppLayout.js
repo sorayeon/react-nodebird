5cf6a8be58115641efb34e37dcb0c99806fef0fd
jihwa <jihwan77@gmail.com>
2021-01-17 10:15:57 +0900

    defaultSelectedKeys change

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -2,11 +2,11 @@ import React, { useState, useCallback } from 'react';
 import ProTypes from 'prop-types';
 import Link from 'next/link';
 import {
-  Layout, Menu, Input, Row, Col, Breadcrumb,
+  Layout, Menu, Input, Row, Col,
 } from 'antd';
 import styled, { createGlobalStyle } from 'styled-components';
 import { useSelector } from 'react-redux';
-import Router from 'next/router';
+import Router, { useRouter } from 'next/router';
 import UserProfile from './UserProfile';
 import LoginForm from './LoginForm';
 
@@ -35,19 +35,20 @@ const AppLayout = ({ children }) => {
   const onSearch = useCallback(() => {
     Router.push(`/hashtag/${searchInput}`);
   }, [searchInput]);
+  const router = useRouter();
 
   return (
     <Layout className="layout">
       <Global />
       <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-          <Menu.Item key="1">
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={[router.pathname]}>
+          <Menu.Item key="/">
             <Link href="/"><a>노드버드</a></Link>
           </Menu.Item>
-          <Menu.Item key="2">
+          <Menu.Item key="/profile">
             <Link href="/profile"><a>프로필</a></Link>
           </Menu.Item>
-          <Menu.Item key="3">
+          <Menu.Item key="/search">
             <SearchInput
               enterButton
               value={searchInput}
@@ -61,10 +62,10 @@ const AppLayout = ({ children }) => {
         <div style={{ minHeight: '400px', padding: '24px', backgroundColor: '#FFF' }}>
           {/* gutter 컬럼 사이의 간격 */}
           <Row gutter={12}>
-            <Col xs={24} sm={6} md={6} style={{ paddingTop: '12px' }}>
+            <Col xs={24} sm={24} md={8} lg={4} style={{ paddingTop: '12px' }}>
               {me ? <UserProfile /> : <LoginForm />}
             </Col>
-            <Col xs={24} sm={18} md={18} style={{ paddingTop: '12px' }}>
+            <Col xs={24} sm={24} md={16} lg={20} style={{ paddingTop: '12px' }}>
               {children}
             </Col>
           </Row>
